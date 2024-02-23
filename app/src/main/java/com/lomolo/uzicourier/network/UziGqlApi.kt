0546e84094b9979415f267347fcf71731dca46c3
edwinlomolo <workockmoses@gmail.com>
2024-02-23 13:59:27 +0300

    .schema.graphql

--- app/src/main/java/com/lomolo/uzicourier/network/UziGqlApi.kt
+++ app/src/main/java/com/lomolo/uzicourier/network/UziGqlApi.kt
@@ -8,7 +8,9 @@ import com.lomolo.uzicourier.GetCourierDocumentsQuery
 import com.lomolo.uzicourier.GetCourierTripQuery
 import com.lomolo.uzicourier.SetCourierStatusMutation
 import com.lomolo.uzicourier.TrackCourierGpsMutation
+import com.lomolo.uzicourier.TripAssignmentSubscription
 import com.lomolo.uzicourier.type.UploadFile
+import kotlinx.coroutines.flow.Flow
 
 interface UziGqlApiInterface {
     suspend fun getCourierDocuments(): ApolloResponse<GetCourierDocumentsQuery.Data>
@@ -16,36 +18,29 @@ interface UziGqlApiInterface {
     suspend fun trackCourierGps(gps: LatLng): ApolloResponse<TrackCourierGpsMutation.Data>
     suspend fun setCourierStatus(status: String): ApolloResponse<SetCourierStatusMutation.Data>
     suspend fun getCourierTrip(): ApolloResponse<GetCourierTripQuery.Data>
+    fun tripAssignment(userId: String): Flow<ApolloResponse<TripAssignmentSubscription.Data>>
 }
 
 class UziGqlApiRepository(
     private val apolloClient: ApolloClient
 ): UziGqlApiInterface {
-    override suspend fun getCourierDocuments(): ApolloResponse<GetCourierDocumentsQuery.Data> {
-        return apolloClient.query(GetCourierDocumentsQuery()).execute()
-    }
+    override suspend fun getCourierDocuments() = apolloClient.query(GetCourierDocumentsQuery()).execute()
 
-    override suspend fun createCourierDocument(type: UploadFile, uri: String): ApolloResponse<CreateCourierDocumentMutation.Data> {
-        return apolloClient.mutation(
+    override suspend fun createCourierDocument(type: UploadFile, uri: String) = apolloClient.mutation(
             CreateCourierDocumentMutation(uri = uri, type = type)
         ).execute()
-    }
 
-    override suspend fun trackCourierGps(gps: LatLng): ApolloResponse<TrackCourierGpsMutation.Data> {
-        return apolloClient.mutation(
+    override suspend fun trackCourierGps(gps: LatLng) = apolloClient.mutation(
             TrackCourierGpsMutation(gps.latitude, gps.longitude)
         ).execute()
-    }
 
-    override suspend fun setCourierStatus(status: String): ApolloResponse<SetCourierStatusMutation.Data> {
-        return apolloClient.mutation(
+    override suspend fun setCourierStatus(status: String) = apolloClient.mutation(
             SetCourierStatusMutation(status)
         ).execute()
-    }
 
-    override suspend fun getCourierTrip(): ApolloResponse<GetCourierTripQuery.Data> {
-        return apolloClient.query(
+    override suspend fun getCourierTrip() = apolloClient.query(
             GetCourierTripQuery()
         ).execute()
-    }
+
+    override fun tripAssignment(userId: String) = apolloClient.subscription(TripAssignmentSubscription(userId)).toFlow()
 }
