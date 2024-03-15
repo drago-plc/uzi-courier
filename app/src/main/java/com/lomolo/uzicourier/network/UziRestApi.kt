e5c5d09e6481ae3ad32aa77680ff96369881a95c
edwinlomolo <workockmoses@gmail.com>
2024-03-15 12:27:02 +0300

    fix(rest-api): api endpoint

--- app/src/main/java/com/lomolo/uzicourier/network/UziRestApi.kt
+++ app/src/main/java/com/lomolo/uzicourier/network/UziRestApi.kt
@@ -12,16 +12,16 @@ import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface UziRestApiServiceInterface {
-    @GET("v1/ipinfo")
+    @GET("api/ipinfo")
     suspend fun getIpinfo(): Ipinfo
     @Headers("Content-Type: application/json")
-    @POST("v1/signin")
+    @POST("api/signin")
     suspend fun signIn(@Body input: SignIn): Session
     @Headers("Content-Type: application/json")
-    @POST("v1/user/onboard")
+    @POST("api/user/onboard")
     suspend fun onboardUser(@Body input: SignIn): Session
     @Multipart
-    @POST("v1/courier/upload/document")
+    @POST("api/courier/upload/document")
     suspend fun uploadImage(@Part body: MultipartBody.Part): UploadRes
 }
 
