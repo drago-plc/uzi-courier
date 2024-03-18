79130b6df9509624e21cf783b6cb04b1f2753f01
edwinlomolo <workockmoses@gmail.com>
2024-03-18 15:18:45 +0300

    chore: cleanup

--- app/src/main/java/com/lomolo/uzicourier/compose/home/Trip.kt
+++ app/src/main/java/com/lomolo/uzicourier/compose/home/Trip.kt
@@ -1,6 +1,5 @@
 package com.lomolo.uzicourier.compose.home
 
-import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
